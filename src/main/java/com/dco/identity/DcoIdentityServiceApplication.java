c352096c49b7353e43549d58f85d604f7d9866b3
akkimadhava <madhavareddy0205@gmail.com>
2025-02-13 05:00:10 +0530

    initial commit

---
+++ src/main/java/com/dco/identity/DcoIdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dco.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcoIdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcoIdentityServiceApplication.class, args);
+	}
+
+}
